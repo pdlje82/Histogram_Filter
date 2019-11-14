fcbec37545cd2538cf0a2596f4a25121984d1c73
Jan Engelke <jan.engelke@gmx.net>
2019-11-14 12:16:01 +0100

    added getters and setters

--- matrix.cpp
+++ matrix.cpp
@@ -45,3 +45,32 @@ Matrix::Matrix(std::vector<std::vector<float> > initial_grid) {
     rows = initial_grid.size();
     cols = initial_grid[0].size();
 }
+/*
+** TODO: Define a function setGrid()
+**   INPUTS: a 2-D vector
+**   OUPUTS: void
+**
+**   This function receives a 2-D vector representing a matrix, and then
+**    updates the grid, rows, and cols variables
+**
+*/
+void Matrix::setGrid(std::vector <std::vector <float> > new_grid) {
+    grid = new_grid;
+    rows = new_grid.size();
+    cols  = new_grid[0].size();
+}
+/*
+** TODO: Define getGrid(), getRows(), and getCols() functions.
+**    INPUTS: None of these functions have inputs
+**    OUPUTS: Each function should return its respective variable
+**            For example, getRows returns the rows variable
+*/
+std::vector <std::vector <float> > Matrix::getGrid() {
+    return grid;
+}
+std::vector <float>::size_type Matrix::getRows() {
+    return rows;
+}
+std::vector <float>::size_type Matrix::getCols() {
+    return cols;
+}
